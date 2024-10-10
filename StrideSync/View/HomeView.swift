84664645a67fff4e2a9ed1b01df7d4374a3a60c7
johnnybasgallop <johnnybasgallopwork@outlook.com>
2024-10-10 22:24:18 +0100

    added in the title

--- StrideSync/View/HomeView.swift
+++ StrideSync/View/HomeView.swift
@@ -7,30 +7,43 @@ var screenHeight = UIScreen.main.bounds.height
 struct HomeView: View {
     @EnvironmentObject var viewModel: HomeViewModel
     
-    var body: some View {
-        List {
-            VStack(spacing: 10) {
-                StepsWidget().environmentObject(viewModel)
-                
-                HStack(spacing: 10) {
-                    DistanceWidget().environmentObject(viewModel)
-                    FlightsWidget().environmentObject(viewModel)
+    var body: some View{
+        VStack(spacing: 0){
+            HStack(alignment:.center){
+                Text("StrideSync")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+                Image("Logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 40)
+                    .offset(y:-5)
+            }.padding(.horizontal, 20)
+            List {
+                VStack(spacing: 10) {
+                    StepsWidget().environmentObject(viewModel)
+                    
+                    HStack(spacing: 10) {
+                        DistanceWidget().environmentObject(viewModel)
+                        FlightsWidget().environmentObject(viewModel)
+                    }
+                    .padding(.horizontal)
+                    
+                    StepComparisonView().environmentObject(viewModel)
                 }
-                .padding(.horizontal)
+                .listRowBackground(Color.clear)
+                .listRowInsets(EdgeInsets())
                 
-                StepComparisonView().environmentObject(viewModel)
             }
-            .listRowBackground(Color.clear)
-            .listRowInsets(EdgeInsets())
-            
-        }
-        .frame(width: screenWidth)
-        .scrollContentBackground(.hidden)
-        .refreshable {
-            viewModel.fetchDailyData()
-        }
-        .onAppear {
-            viewModel.fetchDailyData()
+            .frame(width: screenWidth)
+            .scrollContentBackground(.hidden)
+            .refreshable {
+                viewModel.fetchDailyData()
+            }
+            .onAppear {
+                viewModel.fetchDailyData()
+            }
         }
     }
 }
